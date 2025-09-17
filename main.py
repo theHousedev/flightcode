a263aa85423cc0f7ab180c31ea5406e0093480be
kh <kylemhouseholder@gmail.com>
2025-09-17 13:09:23 -0500

    WIP: descent planning

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class CruiseData:
     galsPerHr: float
-    groundspeed: float
+    gndspeed: float
     distance: float
 
 @dataclass
@@ -11,22 +11,39 @@ class FlightData:
     hrsETE: float
     gals: float
 
+@dataclass
+class DescentData:
+    offsetTOD: float    # miles before target to begin descent
+    rateFPM: int        # feet/min descent rate
 
-def getFlightData(input: CruiseData) -> FlightData:
-    hrs = input.distance / input.groundspeed
-    fuelBurn = hrs * input.galsPerHr
-    return FlightData(hrsETE = hrs, gals = fuelBurn)
-
+# Cruise flight calculations
 def promptCruiseData() -> FlightData:
     gph = float(input("Enter fuel burn rate (gal/hr): "))
     gs = float(input("Enter planned groundspeed: "))
     dist = float(input("Enter leg length (nautical miles): "))
-    flightData = getFlightData(CruiseData(gph, gs, dist))
+    
+    hrs = dist / gs
+    fuelBurn = hrs * gph
+    return FlightData(hrsETE = hrs, gals = fuelBurn)
 
-    return flightData
+# Descent planning calculations
+def promptDescentPlan() -> DescentData:
+    altCurrent = float(input("Current altitude: "))
+    altTarget = float(input("Target altitude: "))
+    angle = float(input("Target descent angle (default 3.0°): "))
+
+    altToDescend = altCurrent - altTarget
+
 
 
 def main():
+    # present offer for type of calculation:
+    #   - cruise (fuel burn/ETE)
+    #   - performance (TO/LDG dist, climb rate)
+    #   - '3:1 rule' descent planning
+    #   - gross weight speed recalculation
+    #   - weight shift formula?
+
     cruise = promptCruiseData()
     print(f"Estimated time enroute: {cruise.hrsETE:.2f}hrs")
     print(f"Fuel consumed enroute: {cruise.gals:.1f}gals")
